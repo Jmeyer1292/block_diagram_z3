a152300c7fdebe7e45a7e1d3bd0b7ebd4b70eb6a
Jonathan Meyer <jmeyer@foxbots.co>
2021-05-29 16:57:52 -0500

    Expanded some of the program tests but we've got a long way to go

--- fbdplc/graph.py
+++ fbdplc/graph.py
@@ -10,18 +10,25 @@ class VariableResolver:
     def __init__(self):
         self.data = {}
 
+    @staticmethod
+    def ir_name(name: str, access_no: int) -> str:
+        return f'{name}_${access_no}'
+
+    def list_variables(self):
+        return [k for k in self.data]
+
     def write(self, name):
         if name in self.data:
             self.data[name] += 1
-            return f'{name}_${self.data[name]}'
+            return self.ir_name(name, self.data[name])
         else:
             self.data[name] = 0
-            return f'{name}_$0'
+            return self.ir_name(name, 0)
 
     def read(self, name):
         data = self.data.get(name, None)
         if data is None:
             self.data[name] = 0
             data = 0
-        return f'{name}_${data}'
+        return self.ir_name(name, data)
 
--- tests/test_modeling.py
+++ tests/test_modeling.py
@@ -4,3 +4,6 @@ import fbdplc.modeling as modeling
 def test():
     net = parse_from_file('testdata/simple_or.xml')[0]
     program, ssa = modeling.program_model(net)
+    # This program has 3 named variables (i.e. not temporaries)
+    assert(len(ssa.list_variables()) == 3)
+    print(program)
