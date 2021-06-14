8af4ffa69c49d7ec512f1a07f4c7680342b382d8
Jonathan Meyer <jmeyer@foxbots.co>
2021-06-13 23:41:21 -0500

    FIXUPS

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,13 +1,29 @@
 
 
-from fbdplc.s7xml import parse_block, parse_from_file, _remove_namespaces
-from lxml import etree
+from fbdplc.functions import Call
+from fbdplc.s7xml import parse_function_from_file
 
-def test():
-    root = etree.parse('testdata/UserAnd.xml')
-    root = _remove_namespaces(root)
-    block = parse_block(root)
+import z3
 
-    print(block.name)
-    print(block.variables)
-    print(block.networks)
+
+def test_parse():
+    block = parse_function_from_file('testdata/UserAnd.xml')
+    assert block.name == 'UserAnd'
+    assert len(block.variables.input) == 2
+    assert len(block.variables.output) == 1
+    assert len(block.variables.inout) == 0
+
+
+def test_call():
+    ctx = z3.Context()
+    block = parse_function_from_file('testdata/UserAnd.xml')
+
+    call0 = Call('UserAnd#0')
+    model0 = call0.instantiate('', ctx, block)
+    assert len(model0.ports) == 3
+    print(model0.ports)
+    for k in model0.ports:
+        print(model0.ports[k].name)
+
+
+# TODO(Jmeyer): Test scopes
