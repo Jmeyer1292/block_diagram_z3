a1db284ebb2f7c287a1463f4b02759ae152d0f12
Jonathan Meyer <jmeyer@foxbots.co>
2021-06-20 10:54:27 -0500

    Cleanup of app

--- fbdplc/apps/parse_s7xml.py
+++ fbdplc/apps/parse_s7xml.py
@@ -1,15 +1,23 @@
+from fbdplc.functions import Program
 import sys
-from fbdplc.s7xml import parse_from_file
+from fbdplc.s7xml import parse_from_file, parse_function_from_file
 from fbdplc.modeling import program_model
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('usage: python parse_s7xml.py <PATH>')
     else:
-        path = sys.argv[1]
-        print(f'Attempting to load s7xml file "{path}"')
-        contexts = parse_from_file(sys.argv[1])
-        print(f'Parsed {len(contexts)} networks')
-        solver, ssa = program_model(contexts[0])
-        print(f'Program uses the following variables: {ssa.list_variables()}')
-        print(f'Program Model:\n{solver.assertions()}')
+        program = Program('main')
+        
+        main = None
+        for path in sys.argv[1:]:
+
+            print(f'Attempting to load s7xml file "{path}"')
+            block = parse_function_from_file(path)
+            program.blocks[block.name] = block
+
+            if main is None:
+                main = block    
+        program.entry = main.name
+
+        model = program_model(program)
