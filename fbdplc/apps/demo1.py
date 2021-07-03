99ed59b4e626d1aec571796337d5fbd3093d9b80
Jonathan Meyer <Jmeyer1292@gmail.com>
2021-07-03 00:11:36 -0500

    cleanup of app

--- fbdplc/apps/demo1.py
+++ fbdplc/apps/demo1.py
@@ -1,59 +1,73 @@
-from fbdplc.graph import merge_scopes
-from fbdplc.s7xml import parse_from_file
-from fbdplc.modeling import program_model
-import functools
 
+from fbdplc.analysis import _memory_dict, run_assertions
+from fbdplc.s7xml import parse_function_from_file
+from fbdplc.functions import Program
+from fbdplc.modeling import program_model
 import z3
 
 
-def merge_nets(nets):
-    return functools.reduce(merge_scopes, nets)
-
-
-def run_assertion(program: z3.Solver, assertion_list):
-    test_asserts = z3.Or([z3.Not(a) for a in assertion_list])
-
-    try:
-        program.push()
-        program.add(test_asserts)
-        print('Testing assertions:\n{}'.format(test_asserts))
-        check = program.check()
-        if check == z3.sat:
-            print('Program failed!')
-            print(program.model())
-        elif check == z3.unsat:
-            print('Program PASSED. No path to violated assertion.')
-        else:
-            print('Solver could not definitely determine an answer')
-    finally:
-        program.pop()
+def _load_block(program: Program, target_path: str):
+    block = parse_function_from_file(target_path)
+    program.blocks[block.name] = block
+    return block.name
 
 
 def main():
-    path = 'testdata/example1/Demo.xml'
-    print(f'Attempting to load s7xml file "{path}"')
-    contexts = parse_from_file(path)
-    print(f'Parsed {len(contexts)} networks')
-    solver, ssa = program_model(merge_nets(contexts))
-    print(f'Program uses the following variables: {ssa.list_variables()}')
-    print(f'Program Model:\n{solver.assertions()}')
+    '''
+    See the code in testdata/example1/
+    '''
+    # First create a program and load various block files into it
+    program = Program('example1')
+    # Note that you must set one module as the entry point for the analysis
+    program.entry = _load_block(program, 'testdata/example1/Demo.xml')
+    # The following line translates the XML into a model that z3 can consume
+    model = program_model(program)
 
-    # Our core principles:
-    # Program uses the following variables: ['is_slow', 'mute_req', 'mute_granted',
-    # 'left_clear', 'right_clear', 'scanners_ok', 'estop_clear', 'stop']
+    # We want to write some tests against this code. You can use the model to
+    # "execute" the program by writing to all of the inputs. Then you can check
+    # that you got the expected results:
+    # TODO
 
-    # not estop clear => stop
-    estop_clear = z3.Bool(ssa.read('estop_clear'))
-    stop = z3.Bool(ssa.read('stop'))
-    assert_stops_on_estop = z3.Implies(z3.Not(estop_clear), stop)
-    # (not(left_clear) or not(right_clear)) AND not mute_granted => stop
-    left_clear = z3.Bool(ssa.read('left_clear'))
-    right_clear = z3.Bool(ssa.read('right_clear'))
-    mute_granted = z3.Bool(ssa.read('mute_granted'))
-    scanner_violated = z3.And(
-        z3.Or(z3.Not(left_clear), z3.Not(right_clear)), z3.Not(mute_granted))
-    assert_stop_on_scanners = z3.Implies(scanner_violated, stop)
-    run_assertion(solver, [assert_stops_on_estop, assert_stop_on_scanners])
+    # You can also try to prove more general things about the program.
+    # Lets say we want to prove that the estop being inactive implies a stop is
+    # issued:
+    estop_clear = model.root.read('estop_clear')
+    stop_commanded = model.root.read('stop')
+    estop_implies_stop = z3.Implies(z3.Not(estop_clear), stop_commanded)
+
+    success, counter_example = run_assertions(
+        model, [], [estop_implies_stop, ])
+
+    print(
+        f'"Estop Implies Stop" Assertion Results:\nSuccess := {success}\nCounter-Example := {counter_example}')
+
+    # Great; We've shown that indeed this is true.
+    # Let's try something a little more complex: We have safety sensors that sometimes can be
+    # muted, or disabled. This muting should only happen when other protective measures are
+    # put in place.
+    left, right, mute = [model.root.read(
+        x) for x in ['left_clear', 'right_clear', 'mute_granted']]
+    # The system is safe if muted or both sensors are reporting safe
+    sensors_safe = z3.Or(mute, z3.And(left, right))
+    sensor_failure_implies_stop = z3.Implies(
+        z3.Not(sensors_safe), stop_commanded)
+
+    success, counter_example = run_assertions(
+        model, [], [sensor_failure_implies_stop, ])
+    print(
+        f'"Sensors Safe" Assertion Results:\nSuccess := {success}\nCounter-Example := {counter_example}')
+
+    # Turns out this one fails. There's a typo in the program where one of the
+    # sensors is using the mute_requested variable instead of the mute_granted.
+    print('top level memory print out:')
+    print(_memory_dict(counter_example, model.root))
+    # As you can see, the right scanner is breached and the machine is moving quickly
+    # so the request for mute is not granted but we still didn't command a stop.
+    #   {   'is_slow': False, 'left_clear': True,
+    #       'right_clear': False, 'mute_req': True,
+    #       'estop_clear': True, 'stop': False,
+    #       'mute_granted': False, 'scanners_ok': True
+    #    }
 
 
 if __name__ == '__main__':
