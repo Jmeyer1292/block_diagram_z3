8f1b8c794bb7515d507433be20df2832328d17df
Jonathan Meyer <jmeyer@foxbots.co>
2021-07-16 18:54:20 -0500

    Added a few new data structures of more complex type to the parser test set.

--- tests/test_s7db.py
+++ tests/test_s7db.py
@@ -33,3 +33,28 @@ def test_udt():
     assert len(symbols) == 2
     assert 'min' in symbols
     assert 'max' in symbols
+
+def test_udt_array_with_defaults():
+    '''
+    Parses a data structure with an array with declared defaults like so:
+    v : Array[0..15] of Bool := [false, true];
+    '''
+    pass
+
+def test_udt_polygon():
+    '''
+    A composite of an integer and an array of UDT
+    '''
+    pass
+
+def test_udt_points():
+    '''
+    Two simple data structures of template<typename T> { x: T, y: T} where T in {Real, Int}
+    '''
+    pass
+
+def test_udt_nested_anon():
+    '''
+    A data structure with two nested anonymous data structures
+    '''
+    pass
