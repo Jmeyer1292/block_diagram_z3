01a10f08d814c20bd5d96f7f586f7e41640e858e
Jonathan Meyer <jmeyer@foxbots.co>
2021-07-18 16:52:29 -0500

    Fixup

--- fbdplc/s7db.py
+++ fbdplc/s7db.py
@@ -1,69 +1,25 @@
-from typing import Dict, Iterator
+from typing import Dict
 import lark
 
-DB_GRAMMAR = r'''
+GRAMMAR = r'''
 
-    start: data_block
-
-    data_block: "DATA_BLOCK" ESCAPED_STRING property? section+ "END_DATA_BLOCK" 
-
-    property: "{" assignment+ "}"
-
-    ?section: var_block
-        |    init_block
-        |    version
-    
-    version: "VERSION :" NUMBER
-
-    var_block: "NON_RETAIN" "VAR" var_decl* "END_VAR"
-
-    var_decl: NAME property? ":" sort ";"
-
-    ?sort: TYPE | struct_block
-
-    struct_block: "Struct" var_decl* "END_STRUCT"
-
-    TYPE: NAME
-        | ESCAPED_STRING
-
-
-
-    init_block: "BEGIN" assignment*
-
-    assignment: ASSIGNMENT_NAME ":=" literal ";"?
-
-    SINGLE_QUOTE_STR: "'" NAME "'"
-
-    literal: ESCAPED_STRING
-           | NUMBER
-           | SINGLE_QUOTE_STR
-
-    ASSIGNMENT_NAME: ("_"|LETTER) ("."|"_"|LETTER|DIGIT)*
-
-    COMMENT: "//" /[^\n]*/ "\n"
-    %ignore COMMENT
-
-    %import common.ESCAPED_STRING
-    %import common.CNAME -> NAME
-    %import common.LETTER
-    %import common.DIGIT
-    %import common.NUMBER
-    %import common.WS
-    %ignore WS
-'''
-
-UDT_GRAMMAR = r'''
-
-    start: type_block
+    start: type_block | data_block
 
     type_block: "TYPE" ESCAPED_STRING property? section+ "END_TYPE" 
 
+    data_block: "DATA_BLOCK" ESCAPED_STRING property? section+ "END_DATA_BLOCK"
+
     property: "{" assignment+ "}"
 
-    ?section: struct_block
-        |    init_block
-        |    version
+    ?section:   struct_block
+        |       version
+        |       var_block
+        |       init_block
+
+    init_block: "BEGIN" assignment*
     
+    var_block: "NON_RETAIN" "VAR" var_decl* "END_VAR"
+
     version: "VERSION :" NUMBER
 
     struct_block: "STRUCT"i var_decl* "END_STRUCT"i ";"?
@@ -80,8 +36,6 @@ UDT_GRAMMAR = r'''
     
     TYPE: NAME
         | ESCAPED_STRING
-
-    init_block: "BEGIN" assignment*
 
     assignment: ASSIGNMENT_NAME ":=" literal ";"?
 
@@ -108,8 +62,7 @@ UDT_GRAMMAR = r'''
     %ignore WS
 '''
 
-db_parser = lark.Lark(DB_GRAMMAR)
-udt_parser = lark.Lark(UDT_GRAMMAR)
+db_parser = lark.Lark(GRAMMAR)
 
 
 def iter_children_pred(predicate, iter: lark.Tree):
@@ -117,13 +70,15 @@ def iter_children_pred(predicate, iter: lark.Tree):
         if predicate(x):
             yield x
 
+
 def _parse_array(node: lark.Tree):
     # Pull out data from an 'array_block'
     return {
         'index_begin': node.children[0].value,
         'index_end': node.children[1].value,
-        'type': node.children[2].value,        
+        'type': node.children[2].value,
     }
+
 
 def _parse_decl(decl: lark.Tree):
     '''
@@ -142,10 +97,13 @@ def _parse_decl(decl: lark.Tree):
 
     entry = {'name': None, 'type': None, 'kind': None}
 
-    pred_names = lambda x: isinstance(x, lark.Token) and x.type == 'NAME'
-    pred_types = lambda x: isinstance(x, lark.Token) and x.type == 'TYPE'
-    pred_array = lambda x: isinstance(x, lark.Tree) and x.data == 'array_block'
-    pred_struct = lambda x: isinstance(x, lark.Tree) and x.data == 'struct_block'
+    def pred_names(x): return isinstance(x, lark.Token) and x.type == 'NAME'
+    def pred_types(x): return isinstance(x, lark.Token) and x.type == 'TYPE'
+
+    def pred_array(x): return isinstance(
+        x, lark.Tree) and x.data == 'array_block'
+    def pred_struct(x): return isinstance(
+        x, lark.Tree) and x.data == 'struct_block'
 
     names = list(iter_children_pred(pred_names, decl))
     types = list(iter_children_pred(pred_types, decl))
@@ -203,7 +161,7 @@ def _walk_db(tree: lark.Tree):
     for inits in tree.find_data('init_block'):
         for assignment in inits.children:
             name = assignment.children[0].value
-            value = assignment.children[1].children[0].value
+            value = assignment.children[1].value
             initializers[name] = value
 
     return {
@@ -268,4 +226,4 @@ def parse_udt_file(path: str):
     '''
     See ~parse_db_file() documentation; Called on a user-defined-datatype, or UDT.
     '''
-    return _parse_file(path, udt_parser, _walk_udt)
+    return _parse_file(path, db_parser, _walk_udt)
